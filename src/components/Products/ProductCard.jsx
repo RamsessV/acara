fe743702fe85da4cbee3bfbf84901c41c6fb7b81
RamsessV <jvelasco27@ucol.mx>
2025-09-18 18:05:31 -0600

    18/09/25 2

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -12,7 +12,8 @@ export default function ProductCard({ product }) {
 							</div>
 							<div className="card-body d-flex flex-column">
 								<h5 className="card-title fs-6 fw-bold">{product.name}</h5>
-								<p className="card-text text-muted small mb-2">${product.precio}</p>
+								<p className="card-text text-muted small mb-2 fw-bold">${product.precio}</p>
+								<button className="btn w-75 mx-auto bg-main fw-bold">Ver</button>
 							</div>
 						</div>
 					</div>
